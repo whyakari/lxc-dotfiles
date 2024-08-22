4732c0bc2d5da4fd8de8b8dc69631a073a96f078
Akari <akariondev@gmail.com>
2024-08-22 18:24:20 -0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,10 +1,10 @@
 git clone https://github.com/whyakari/lxc-dotfiles
 cd lxc-dotfiles
 
-ln -s /data/data/com.termux/files/usr/var/lib/lxc/ubuntu/config links/config
-ln -s /data/data/com.termux/files/usr/share/lxc/config/common.conf.d/ubuntu.conf links/ubuntu.conf
-ln -s /data/data/com.termux/files/usr/var/lib/lxc/ubuntu/rootfs/etc/shadow links/shadow
-ln -s /data/data/com.termux/files/usr/bin/lxc-setup-cgroups links/lxc-setup-cgroups
+cp links/config /data/data/com.termux/files/usr/var/lib/lxc/ubuntu/config
+cp links/ubuntu.conf /data/data/com.termux/files/usr/share/lxc/config/common.conf.d/ubuntu.conf
+cp links/shadow /data/data/com.termux/files/usr/var/lib/lxc/ubuntu/rootfs/etc/shadow
+cp links/lxc-setup-cgroups /data/data/com.termux/files/usr/bin/lxc-setup-cgroups 
 
 cd ..
 rm -rf lxc-dotfiles
