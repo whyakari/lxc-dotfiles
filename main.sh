64479d7fb210388561c38336700bf16441a500f3
Akari <akariondev@gmail.com>
2024-08-22 17:55:03 -0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,9 @@
+git clone https://github.com/whyakari/lxc-dotfiles
+cd lxc-dotfiles
+
 ln -s /data/data/com.termux/files/usr/var/lib/lxc/ubuntu/config links/config
 ln -s /data/data/com.termux/files/usr/share/lxc/config/common.conf.d/ubuntu.conf links/ubuntu.conf
 ln -s /data/data/com.termux/files/usr/var/lib/lxc/ubuntu/rootfs/etc/shadow links/shadow
 
-
+cd ..
+rm -rf lxc-dotfiles
