b7620683d20b3eba0884095cf88205c5b62de6a0
Shoiya Akari <akariondev@gmail.com>
2024-08-22 17:41:55 -0300

    first version
    Signed-off-by: Shoiya Akari <akariondev@gmail.com>

---
+++ main.sh
@@ -0,0 +1,5 @@
+ln -s /data/data/com.termux/files/usr/var/lib/lxc/ubuntu/config links/config
+ln -s /data/data/com.termux/files/usr/share/lxc/config/common.conf.d/ubuntu.conf links/ubuntu.conf
+ln -s /data/data/com.termux/files/usr/var/lib/lxc/ubuntu/rootfs/etc/shadow links/shadow
+
+
